f545fe4d289746b2e4121dc7538085bd7a92beb2
zdezak <zdezarus@gmail.com>
2021-09-25 22:05:56 +0500

    init app

---
+++ app/src/main/java/com/zdez/getartist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.zdez.getartist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zdez.getartist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}

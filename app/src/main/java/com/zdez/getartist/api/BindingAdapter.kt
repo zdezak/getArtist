58d08f9039c32e06b00ff47752854f0fcebf0f2c
zdezak <zdezarus@gmail.com>
2021-12-05 23:20:22 +0500

    show albums correct

--- app/src/main/java/com/zdez/getartist/api/BindingAdapter.kt
+++ app/src/main/java/com/zdez/getartist/api/BindingAdapter.kt
@@ -4,13 +4,19 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.zdez.getartist.R
 
 @BindingAdapter("url")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
-        val uri = imgUrl.toUri().buildUpon().scheme("https").build()
-        Glide.with(imgView.context)
-            .load(uri)
-            .into(imgView)
+        if (imgUrl.isNullOrEmpty()) {
+            imgView.setImageResource(R.drawable.empty_image)
+        } else {
+            val uri = imgUrl.toUri().buildUpon().scheme("https").build()
+            Glide.with(imgView.context)
+                .load(uri)
+                .into(imgView)
+        }
+
     }
 }
